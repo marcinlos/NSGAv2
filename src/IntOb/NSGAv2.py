fd3a1297385ad7ed9cdd80bc37c8a1508f800bb6
los <losiu99@gazeta.pl>
2014-11-07 21:21:18 +0100

    Extract GA blocks into separate module
    Some elements of NSGA implementation - genetic operations, binary
    tournament selection - are reusable, hence they are hereby extracted to
    genetics.py module.

--- src/IntOb/NSGAv2.py
+++ src/IntOb/NSGAv2.py
@@ -2,46 +2,13 @@
 from random import random, randint, sample, shuffle
 from collections import defaultdict
 from operator import itemgetter, attrgetter, mul
-from utils import lerp, clamp, rand, tossCoin, randVector, maximal, \
+from .utils import lerp, clamp, rand, tossCoin, randVector, maximal, \
     dominates, inverslyDominates, weaklyInverslyDominates, partialSort
+from .genetics import Specimen, randomPopulation, mutation, crossover, select
 
 
-
-class Specimen(object):
-    """ Single element of the population
-    """
-
-    def __init__(self, x):
-        self.x = tuple(x)
-        self.rank = None
-        self.val = None
-        self.crowd = None
-
-    def __str__(self):
-        return '|{}|'.format(self.x)
-
-    def domain_dim(self):
-        return len(self.x)
-
-    def value_dim(self):
-        return len(self.val)
-
-    @staticmethod
-    def dominates(a, b):
-        return dominates(a.val, b.val)
-
-    @staticmethod
-    def crowdedCmp(a, b):
-        return (a.rank, -a.crowd) < (b.rank, -b.crowd)
-
-
-def randomPopulation(size, bounds):
-    """ Creates random initial population - set of points.
-
-    size   - number of points to create
-    bounds - bounds for each dimension
-    """
-    return [Specimen(randVector(bounds)) for _ in xrange(size)]
+def crowdedCmp(a, b):
+    return (a.rank, -a.crowd) < (b.rank, -b.crowd)
 
 
 def computeCrowdingDistance(guys, ranges):
@@ -153,70 +120,6 @@ def hypervolume(p, xs):
 
 # Genetic operators
 
-def mutation(a, p, bounds, max_changes):
-    """ Mutates each component of the vector with probability p, changing it
-    by random amount, uniformly choosen from the symmetric intervals specified
-    by max_changes vector, ensuring the result stays inside the region given
-    by bounds.
-
-    a           - vector to mutate
-    p           - probability of mutation
-    bounds      - solution domain
-    max_changes - maximal acceptable changes due to mutation for each somponent
-
-    Returns: new, modified specimen
-    """
-    b = []
-    for i, x in enumerate(a.x):
-        if tossCoin(p):
-            c = max_changes[i]
-            d = lerp(-c, c, random())
-            m, M = bounds[i]
-            x = clamp(x + d, m, M)
-        b.append(x)
-    return Specimen(b)
-
-
-def crossover(a, b):
-    """ Performs crossover of to specimens, effectively interpolating between
-    their components.
-
-    a, b - specimens to mate
-    Returns: pair of children
-    """
-    c1 = []
-    c2 = []
-    for x, y in zip(a.x, b.x):
-        u = random()
-        f = u #pow(2 * u, 1 / (1 + 0.3))
-
-        c1.append(0.5 * ((1 - f) * x + (1 + f) * y))
-        c2.append(0.5 * ((1 + f) * x + (1 - f) * y))
-    return (Specimen(c1), Specimen(c2))
-
-
-def select(population, compare, N, p):
-    """ Performs selection of N individuals from the population, using binary
-    tournament scheme with pressure p. Concretly, pair of individuals is
-    drawn randomly from the population, they are compared with specified
-    comparator and with probability p the winner is selected.
-
-    population - population to select from
-    compare    - function used to compare individuals, accepts two arguments,
-                 and returns True if the first one is not better than the second
-    N          - number of individuals to select
-    p          - selection pressure
-    """
-    res = []
-    for _ in xrange(N):
-        a, b = sample(population, 2)
-        if compare(a, b):
-            a, b = b, a
-        # Here a >= b
-        choosen = a if tossCoin(p) else b
-        res.append(choosen)
-    return res
-
 
 class NSGA(object):
 
@@ -239,7 +142,7 @@ class NSGA(object):
 
     def createOffspring(self, P, N):
         p = self.params['selection_pressure']
-        Q = select(P, Specimen.crowdedCmp, N, p)
+        Q = select(P, crowdedCmp, N, p)
         shuffle(Q)
 
         cp = self.params['crossover_prob']
---
+++ src/IntOb/genetics.py
@@ -0,0 +1,102 @@
+
+from random import random, sample
+from .utils import dominates, randVector, tossCoin, lerp, clamp
+
+
+class Specimen(object):
+    """ Single element of the population
+    """
+
+    def __init__(self, x):
+        self.x = tuple(x)
+        self.rank = None
+        self.val = None
+        self.crowd = None
+
+    def __str__(self):
+        return '|{}|'.format(self.x)
+
+    def domain_dim(self):
+        return len(self.x)
+
+    def value_dim(self):
+        return len(self.val)
+
+    @staticmethod
+    def dominates(a, b):
+        return dominates(a.val, b.val)
+
+
+def randomPopulation(size, bounds):
+    """ Creates random initial population - set of points.
+
+    size   - number of points to create
+    bounds - bounds for each dimension
+    """
+    return [Specimen(randVector(bounds)) for _ in xrange(size)]
+
+
+def mutation(a, p, bounds, max_changes):
+    """ Mutates each component of the vector with probability p, changing it
+    by random amount, uniformly choosen from the symmetric intervals specified
+    by max_changes vector, ensuring the result stays inside the region given
+    by bounds.
+
+    a           - vector to mutate
+    p           - probability of mutation
+    bounds      - solution domain
+    max_changes - maximal acceptable changes due to mutation for each somponent
+
+    Returns: new, modified specimen
+    """
+    b = []
+    for i, x in enumerate(a.x):
+        if tossCoin(p):
+            c = max_changes[i]
+            d = lerp(-c, c, random())
+            m, M = bounds[i]
+            x = clamp(x + d, m, M)
+        b.append(x)
+    return Specimen(b)
+
+
+def crossover(a, b):
+    """ Performs crossover of to specimens, effectively interpolating between
+    their components.
+
+    a, b - specimens to mate
+    Returns: pair of children
+    """
+    c1 = []
+    c2 = []
+    for x, y in zip(a.x, b.x):
+        u = random()
+        f = u #pow(2 * u, 1 / (1 + 0.3))
+
+        c1.append(0.5 * ((1 - f) * x + (1 + f) * y))
+        c2.append(0.5 * ((1 + f) * x + (1 - f) * y))
+    return (Specimen(c1), Specimen(c2))
+
+
+def select(population, compare, N, p):
+    """ Performs selection of N individuals from the population, using binary
+    tournament scheme with pressure p. Concretly, pair of individuals is
+    drawn randomly from the population, they are compared with specified
+    comparator and with probability p the winner is selected.
+
+    population - population to select from
+    compare    - function used to compare individuals, accepts two arguments,
+                 and returns True if the first one is not better than the second
+    N          - number of individuals to select
+    p          - selection pressure
+    """
+    res = []
+    for _ in xrange(N):
+        a, b = sample(population, 2)
+        if compare(a, b):
+            a, b = b, a
+        # Here a >= b
+        choosen = a if tossCoin(p) else b
+        res.append(choosen)
+    return res
+
