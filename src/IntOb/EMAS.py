c2e3c8b3b8eef601eed3a20f5d56aa25676f5a7b
los <losiu99@gazeta.pl>
2014-11-08 13:30:12 +0100

    Add world & population creation

--- src/IntOb/EMAS.py
+++ src/IntOb/EMAS.py
@@ -1,33 +1,16 @@
 
 from .genetics import Specimen
+from random import sample
 
-
-class Island(object):
-
-    def __init__(self):
-        pass
-
-    def selectAgentForEncounter():
-        pass
-
-    def neighbourIslands():
-        pass
-
-    @property
-    def travel_treshold():
-        pass
-
-    @property
-    def reproduction_treshold():
-        pass
 
 
 class Agent(object):
 
-    def __init__(self, x, energy, env):
+    def __init__(self, x, val, energy, env):
         self.env = env
         self.x = x
         self.energy = energy
+        self.val = val
 
     def can_travel(self):
         return self.energy >= self.env.travel_treshold
@@ -46,3 +29,87 @@ class Agent(object):
 
     def fight():
         pass
+
+
+class Island(object):
+
+    def __init__(self):
+        self.neighbours = []
+        self.inhabitants = set()
+
+    def add_neighbour(self, island):
+        self.neighbours.append(island)
+
+    def add_agent(a):
+        self.inhabitants.add(a)
+
+    def remove_agent(a):
+        self.inhabitants.remove(a)
+
+
+class Env(object):
+
+    def __init__(self, params, island):
+        self.params = params
+        self.island = island
+
+    def pick_encounter(self):
+        return sample(self.island.inhabitants)[0]
+
+    def neighbour_islands(self):
+        return self.island.neighbours
+
+    @property
+    def travel_treshold(self):
+        return self.params['travel_treshold']
+
+    @property
+    def reproduction_treshold(self):
+        return self.params['reproduction_treshold']
+
+
+class EMAS(object):
+    params = {
+        'world_size' : 5,
+        'population_size': 10,
+        'init_energy': 0.5,
+    }
+
+    def __init__(self, fs, bounds, ranges, **params):
+        self.f = lambda x: tuple(f(x) for f in fs)
+        self.bounds = bounds
+        self.ranges = ranges
+        self.params = dict(EMAS.params, **params)
+        self.max_changes = [s * (M - m) for m, M in bounds]
+        self.world = []
+
+    def create_world(self):
+        """ Creates fully connected island graph.
+        """
+        size = self.params['world_size']
+        self.world = [Island() for _ in xrange(size)]
+
+        for a in self.world:
+            for b in self.world:
+                if a is not b:
+                    a.add_neighbour(b)
+
+    def populate_world(self):
+        """ Fills the islands with population_size randomly choosen individuals.
+        """
+        N = self.params['population_size']
+        energy = self.params['init_energy']
+
+        for island in self.world:
+            env = Env(self.params, island)
+            for _ in xrange(N):
+                x = randVector(self.bounds)
+                val = self.f(x)
+                agent = Agent(x, val, energy, env)
+                island.add_agent(agent)
+
+    def optimize(self, steps, callback=None):
+        self.create_world()
+        self.populate_world()
+
+
