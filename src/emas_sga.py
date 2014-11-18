6097db47ed0ca6562b9cb4649e75a4debbb0b75f
los <losiu99@gazeta.pl>
2014-11-18 19:56:37 +0100

    Update SGA optimizer for elEMAS

--- src/emas_sga.py
+++ src/emas_sga.py
@@ -13,6 +13,7 @@ from pyevolve import Selectors
 from pyevolve import Initializators, Mutators
 from pyevolve import Consts
 from random import random, randint, uniform as rand_uniform
+import sys
 
 
 problem = ZDT2
@@ -22,67 +23,90 @@ population_size = 30
 
 
 def eval_func(genome, steps=100):
-    print 'eval'
     F, bounds, ranges, volume = problem()
     refpoint = tuple(r[1] for r in ranges)
+
+    print 'Eval...',
+    sys.stdout.flush()
 
     alg = EMAS(F, bounds, ranges, **genome.conf)
     guys = alg.optimize(steps, None)
 
     vals = [guy.val for guy in guys]
+
+    print 'HVR for {} points...'.format(len(vals)),
+    sys.stdout.flush()
+
     V = hypervolume(refpoint, vals)
+
+    print 'done.'
+    sys.stdout.flush()
+
     hvr = V / volume
     return 100 * hvr
 
-params = {
-    'world_size' : 2,
-    'population_size': 50,
-    'init_energy': 0.5,
-    'fight_transfer': 0.2,
-    'travel_threshold': 0.7,
-    'travel_cost': 0.2,
-    'reproduction_threshold': 0.8,
-    'death_threshold': 0.1,
-    'mutation_probability': 0.2,
-}
+
+def mutate(genome, what, s, m=None, M=None, rand_gen=rand_uniform):
+    val = genome.conf[what] + rand_gen(-s, s)
+
+    if m is not None:
+        val = max(m, val)
+    if M is not None:
+        val = min(M, val)
+
+    genome.conf[what] = val
+
+
+def mutate_uniform(genome, what, s, m=None, M=None):
+    mutate(genome, what, s, m, M, rand_uniform)
+
+
+def mutate_int(genome, what, s, m=None, M=None):
+    mutate(genome, what, s, m, M, randint)
+
 
 def mutator(genome, **kwargs):
     p = kwargs['pmut']
     mutations = 0
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['init_energy'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'init_energy', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['fight_transfer'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'fight_transfer', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['travel_threshold'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'travel_threshold', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['travel_cost'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'travel_cost', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['reproduction_threshold'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'reproduction_threshold', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['death_threshold'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'death_threshold', 0.1)
         mutations += 1
 
     if Util.randomFlipCoin(p):
-        s = 0.1
-        genome.conf['mutation_probability'] += rand_uniform(-s, s)
+        mutate_uniform(genome, 'mutation_probability', 0.1)
+        mutations += 1
+
+    if Util.randomFlipCoin(p):
+        mutate_uniform(genome, 'elite_threshold', 5, m=1)
+        mutations += 1
+
+    if Util.randomFlipCoin(p):
+        mutate_int(genome, 'elite_islands', 3, m=1)
+        mutations += 1
+
+    if Util.randomFlipCoin(p):
+        mutate_int(genome, 'world_size', 3, m=1, M=5)
         mutations += 1
 
     return mutations
@@ -99,13 +123,15 @@ def initializer(genome, **kwargs):
     params = {
         'world_size' : randint(1, 5),
         'population_size': randint(20, 100),
+        'elite_islands': randint(1, 5),
         'init_energy': rand_uniform(0.2, 1),
         'fight_transfer': rand_uniform(0, 1),
         'travel_threshold': rand_uniform(0, 1),
         'travel_cost': rand_uniform(0, 1),
-        'reproduction_threshold': rand_uniform(0.3, 1),
+        'reproduction_threshold': rand_uniform(0, 1),
         'death_threshold': rand_uniform(0.01, 0.4),
         'mutation_probability': rand_uniform(0.05, 0.5),
+        'elite_threshold': rand_uniform(1, 50),
     }
     genome.conf.update(params)
 
